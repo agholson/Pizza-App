2e5d6142c192a0936f621e0ab6bd18643ab76516
agholson <87455899+agholson@users.noreply.github.com>
2021-07-24 22:38:04 -0400

    Initial Commit

---
+++ Challenge7-MVVM/Challenge7_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge7_MVVMApp.swift
+//  Challenge7-MVVM
+//
+//  Created by Shepherd on 7/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge7_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
